f7eefaf5267ac0c059d8b495384b363dac5f161b
Hubert <92681087+hv6erT@users.noreply.github.com>
2025-12-08 21:28:59 +0100

    Modyfikacja struktury modułów

--- main.py
+++
@@ -1,4 +0,0 @@
-
-n = int(input('Wprowadz glowna liczbe kwantowa'))
-l = int(input('Wprowadz poboczna liczbe kwantowa'))
-m = int(input('Wprowadz magnetyczna liczbe kwantowa'))
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from model import State
+
+n = int(input('Wprowadz glowna liczbe kwantowa'))
+l = int(input('Wprowadz poboczna liczbe kwantowa'))
+m = int(input('Wprowadz magnetyczna liczbe kwantowa'))
