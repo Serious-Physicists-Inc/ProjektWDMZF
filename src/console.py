41d2bee0473ec75a5cd36730d43415206c036f93
Hubert <92681087+hv6erT@users.noreply.github.com>
2026-01-24 15:29:20 +0100

    Update console.py

--- src/console.py
+++ src/console.py
@@ -16,7 +16,6 @@ QtWidgets.QApplication.setAttribute(QtCore.Qt.ApplicationAttribute.AA_ShareOpenG
 
 @dataclass
 class Settings:
-    interactive: bool = True
     fps: int = 25
     speed: float = 0.25
     plot_type: Literal['ScatterPlot', 'VolumePlot'] = 'ScatterPlot'
@@ -50,38 +49,37 @@ def main() -> Tuple[Union[ScatterWindow, VolumeWindow], Optional[Scheduler]]:
     plot.show()
 
     scheduler: Optional[Scheduler] = None
-    if settings.interactive:
-        def callback(i: int) -> Union[Scatter, Volume]:
-            return source.val(i * settings.speed).masked()
+    def callback(i: int) -> Union[Scatter, Volume]:
+        return source.val(i * settings.speed).masked()
 
-        scheduler = plot.auto_update(callback, settings.fps)
+    scheduler = plot.auto_update(callback, settings.fps)
 
-        fps_rec = []
-        en_vals = dict(zip(((s.spec.n, s.spec.l, s.spec.m) for s in states), (s.energy_func().ev_val() for s in states)))
-        def on_step(i: int) -> None:
-            nonlocal scheduler
-            nonlocal fps_rec
+    fps_rec = []
+    en_vals = dict(zip(((s.spec.n, s.spec.l, s.spec.m) for s in states), (s.energy_func().ev_val() for s in states)))
+    def on_step(i: int) -> None:
+        nonlocal scheduler
+        nonlocal fps_rec
 
-            fps = scheduler.fps
-            if fps > 0:
-                fps_rec.append(fps)
-                if len(fps_rec) > settings.fps:
-                    fps_rec.pop(0)
+        fps = scheduler.fps
+        if fps > 0:
+            fps_rec.append(fps)
+            if len(fps_rec) > settings.fps:
+                fps_rec.pop(0)
 
-            fps_avg = sum(fps_rec) / len(fps_rec) if fps_rec else 0.0
+        fps_avg = sum(fps_rec) / len(fps_rec) if fps_rec else 0.0
 
-            nonlocal en_vals
-            plot.set_hud(
-                f"speed:{' ' * 12}{settings.speed:.3g}\n"
-                + f"fps:{' ' * 14}{fps_avg:.3g}\nspec:\n"
-                + "\n".join(
-                    f"{' ' * 5}({s.n}, {s.l}, {s.m}):\n"
-                    f"{' ' * 7}en: {en_vals[(s.n, s.l, s.m)]: .6g} eV"
-                    for s in atom.specs
-                )
+        nonlocal en_vals
+        plot.set_hud(
+            f"speed:{' ' * 12}{settings.speed:.3g}\n"
+            + f"fps:{' ' * 14}{fps_avg:.3g}\nspec:\n"
+            + "\n".join(
+                f"{' ' * 5}({s.n}, {s.l}, {s.m}):\n"
+                f"{' ' * 7}en: {en_vals[(s.n, s.l, s.m)]: .6g} eV"
+                for s in atom.specs
             )
+        )
 
-        scheduler.stepOccurred.connect(on_step)
+    scheduler.stepOccurred.connect(on_step)
 
     return plot, scheduler
 
